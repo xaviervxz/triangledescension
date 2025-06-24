b98cd56c4ca632cd05ebd3ac851968e7325b1cfe
Vince Zell <xzviervxz@gmail.com>
2025-06-23 22:15:39 -0400

    Init Commit

---
+++ recursive_tri.gd
@@ -0,0 +1,117 @@
+class_name RecursiveTri extends RegularPoly
+
+var locations : Array[Vector2]
+
+
+
+##### FUNCTIONS
+
+func change_clickability() -> bool:
+	var clarea : Area2D = get_node("ClickableArea")
+	clarea.input_pickable = not clarea.input_pickable
+	return clarea.input_pickable
+
+##### Child-Related Funcs
+func trigger_culling():
+	if name != "ROOT":
+		get_parent().cull_children()
+
+func cull_children():
+	for n in ["L","R","C","T"]:
+		get_node(name+"_"+n).queue_free()
+		
+	change_clickability()
+
+func create_children():
+	change_clickability()
+	var children : Array[RecursiveTri] = []
+	# create leftest
+	var next_name = self.name + "_L"
+	if not get_node(next_name):
+		var leftest_child = create_child()
+		add_child(leftest_child)
+		leftest_child.name = next_name
+		children.append(leftest_child)
+	# create rightest, going right
+	next_name = self.name + "_R"
+	if not get_node(next_name):
+		var rightest_child = create_child()
+		add_child(rightest_child)
+		rightest_child.name = next_name
+		rightest_child.position = Vector2(base_length / 2, 0)
+		children.append(rightest_child)
+	
+	# center & top share an origin point, determined by orientation
+	var left_mid = get_midpoint(3)
+	
+	next_name = self.name + "_C"
+	if not get_node(next_name):
+		var center_child = create_child(false)
+		add_child(center_child)
+		center_child.name = next_name
+		center_child.position = left_mid
+		children.append(center_child)
+	
+	next_name = self.name + "_T"
+	if not get_node(next_name):
+		var top_child = create_child()
+		add_child(top_child)
+		top_child.name = next_name
+		top_child.position = left_mid
+		children.append(top_child)
+	
+	return children
+	
+func create_child(is_same_orientation : bool = true) -> RecursiveTri:
+	var child = RecursiveTri.new()
+	child.num_sides = self.num_sides # always 3 atm
+	child.is_up_tri = self.is_up_tri if is_same_orientation else not self.is_up_tri
+	child.base_length = self.base_length / 2
+	return child
+
+func increase_resolution(levels:int=1):
+	s_log("info: increasing resolution")
+	var cs = create_children()
+	s_log("Created %d Children" % len(cs))
+	pass
+	
+func decrease_resolution(levels:int=1):
+	s_log("info: decreasing resolution")
+	trigger_culling()
+	pass
+
+##### INPUTS
+
+func init() -> RecursiveTri:
+	super.init()
+	
+	var coll = CollisionPolygon2D.new()
+	coll.polygon = self.points_list
+	var area = Area2D.new()
+	area.name = "ClickableArea"
+	area.add_child(coll)
+	add_child(area)
+	
+	area.mouse_entered.connect(_on_clickable_area_mouse_entered)
+	area.mouse_exited.connect(_on_clickable_area_mouse_exited)
+	area.input_event.connect(_on_clickable_area_input_event)
+	
+	return self
+
+func _on_clickable_area_mouse_entered() -> void:
+	var lmp = get_local_mouse_position()
+	var gmp = get_global_mouse_position()
+	s_log("Mouse Entered at point: (%f, %f) / (%f, %f)" % [lmp.x, lmp.y, gmp.x, gmp.y])
+
+
+func _on_clickable_area_mouse_exited() -> void:
+	var lmp = get_local_mouse_position()
+	var gmp = get_global_mouse_position()
+	s_log("Mouse Exited at point: (%f, %f) / (%f, %f)" % [lmp.x, lmp.y, gmp.x, gmp.y])
+	
+
+func _on_clickable_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if  event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		increase_resolution()
+	if  event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		decrease_resolution()
---
+++ regular_poly.gd
@@ -0,0 +1,71 @@
+class_name RegularPoly extends Node2D
+
+@export var is_up_tri : bool = true
+@export var base_length : float = 256
+@export var num_sides : int = 3
+@export var points_list : PackedVector2Array
+
+func s_log(text : String, lvl : String = "INFO"):
+	print("LOG: %s: %s >> %s" %[lvl, self.name, text])  
+	
+	
+func get_reg_angle(side_count : int) -> float:
+	assert( side_count >= 2, "ERROR: You must provide a reasonable number of sides.");
+	var total_angle = (side_count - 2) * PI
+	var side_angle = total_angle / side_count
+	print("-- side_angle: %f" % side_angle)
+	return PI - side_angle
+	
+func create_reg_polygon(angle : float, steps : int, base_vec : Vector2) -> Polygon2D:
+	print("-- Creating Poly with %d steps of angle %f" % [steps, angle])
+	var poly = Polygon2D.new()
+	poly.name="Equilateral"
+	poly.color = _generate_random_hsv_color()
+	var cursor = Vector2(0,0)
+	var points = [cursor]
+	while steps > 0:
+		cursor = cursor + base_vec
+		print("-- -- Adding Point %f,%f" % [cursor.x, cursor.y])
+		points.append(cursor)
+		base_vec = base_vec.rotated(angle)
+		steps -= 1
+	points_list = PackedVector2Array(points)
+	poly.set_polygon(points_list)
+	return poly
+
+func _generate_random_hsv_color() -> Color:
+	return Color.from_hsv(
+		randf(), # HUE
+		randf_range(0.6, 1), # SATURATION
+		randf_range(0.9, 1.0), # BRIGHTNESS
+	)
+	
+	# 1 indexed
+func get_midpoint(side : int):
+	assert( side <= num_sides and side > 0, "ERROR: There are %d sides, but you asked for the midpoint of side #%d!" % [num_sides, side]);
+	var sides = points_list
+	print(points_list)
+	sides.append(points_list[0]) 
+	var start = points_list[side - 1]
+	var end = points_list[side]
+	var mp = ((start - end) / 2) + end
+	s_log("MP CALC: (s=%d) (%f,%f) -- (%f,%f) -- (%f,%f)" %[side, start.x, start.y, mp.x, mp.y, end.x, end.y])
+	return mp
+	
+		
+func init():
+	# is drawn from either bottom left or upper left corner
+	var angle = get_reg_angle(num_sides)
+	if is_up_tri:
+		angle = angle * -1
+	
+	var base_side_vector = Vector2(base_length, 0)
+	
+	add_child(create_reg_polygon(angle, num_sides-1, base_side_vector))
+	print("Created Poly with %d sides of len %d" % [num_sides, base_length])
+	
+	return self
+	
+func _ready() -> void:
+	init()
+	print("** tris ready **")
